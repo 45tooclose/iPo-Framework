1f03f910a2b638405b8b43522f645afb8c16cef6
unknown <m.faussurier@gda.fr>
2017-10-15 15:27:34 +0200

    r() function

--- core/autoloader.inc.php
+++ core/autoloader.inc.php
@@ -5,27 +5,27 @@
 */
 spl_autoload_register(
         function ($class) {
-            echo("Loading :".$class);
         $pieces = preg_split('/(?=[A-Z])/',$class);
         switch($pieces[count($pieces) - 1]){
             //If classename like UsersMasterModel
             //-> we include models/UsersMasterModel.php
             case 'Model' :
-                $path = 'models/' . $class . '.php';        
+                $path = './core/models/' . $class . '.php';        
                 break;
             //If classename like UsersMasterController
             //-> we include controllers/UsersMasterController.php
             case 'Controller' :
-                $path = 'controllers/' . $class . '.php';
+                $path = './core/controllers/' . $class . '.php';
                 break;
             //Else, we include classes/$class.php
             default :
-                $path = 'classes/' . $class . '.class.php';
+                $path = './core/classes/' . $class . '.class.php';
                 break;
         }
-        include $path;
-    }
-);
-
-
-?>
+        if(include($path)){
+            r("Success fully loaded : ".$class);
+        }else{
+            r("Error while loading : ".$class);                        
+        }
+        
+    });
--- core/classes/Core.class.php
+++ core/classes/Core.class.php
@@ -3,16 +3,6 @@ class Core {
 
     public $config;
 
-    static public function Dbg($string, string $varname = "",bool $die = false){
-        echo("<br>Type: ".gettype($string)." ".$vaarname."<br>");
-        highlight_string("<?php\n\$DbgOut =\n" . var_export($string, true) . ";\n?>");
-        echo("<br>");        
-        if($die)
-        {
-            exit();
-        }
-    }
-
     static public function print_var_name($var) {
         foreach($GLOBALS as $var_name => $value) {
             if ($value === $var) {
@@ -23,7 +13,6 @@ class Core {
         return false;
     }
     static public function Init(){
-        Core::Dbg($_GET);
         $config = new Config();
         $core = new self($config);
     }
