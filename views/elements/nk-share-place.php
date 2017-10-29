98a22e5ce49b919dd8c91d02c9539573b5472265
unknown <m.faussurier@gda.fr>
2017-10-29 12:44:54 +0100

    assets

---
+++ views/elements/nk-share-place.php
@@ -0,0 +1,20 @@
+
+    <!--
+    START: Share Place
+
+    Additional Classes:
+        .nk-share-place-light
+-->
+<div class="nk-share-place">
+        <div>
+            <div class="container">
+                <div class="nk-share-icons" data-url="https://shaiya.eu/">
+                    <div class="plusone nk-share-icon fa fa-google-plus" title="Share page on Google+"></div>
+                    <div class="facebook nk-share-icon fa fa-facebook" title="Share page on Facebook"></div>
+                    <div class="twitter nk-share-icon fa fa-twitter" title="Share page on Twitter"></div>
+                    <div class="pinterest nk-share-icon fa fa-pinterest" title="Share page on Pinterest"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END: Share Place -->
